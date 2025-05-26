4611754fe177b60690ee5b980c022c8c3981847d
Marcela <lmvt.07@gmail.com>
2025-05-26 09:44:44 -0600

    create shell for books controllers

--- controllers/books.js
+++ controllers/books.js
@@ -1,7 +1,8 @@
 function createBook(req, res) {
+  const body = req.body;
   res.json({
     _id: "1234",
-    title: "titi",
+    title: "titi.......",
   });
 }
 
@@ -14,7 +15,37 @@ function getBooks(req, res) {
   ]);
 }
 
+function getBookById(req, res) {
+  const bookId = req.params.id;
+  console.log(bookId);
+  res.json({
+    _id: "1234",
+    title: "cocacola",
+  });
+}
+
+function updateBookById(req, res) {
+  const bookId = req.params.id;
+  const body = req.body;
+  console.log(bookId, body);
+  res.json({
+    _id: "1234",
+    title: "titi",
+  });
+}
+
+function deleteBookById(req, res) {
+  const bookId = req.params.id;
+  res.json({
+    _id: "1234",
+    title: "titi",
+  });
+}
+
 module.exports = {
   createBook,
   getBooks,
+  getBookById,
+  updateBookById,
+  deleteBookById,
 };
--- routes/books.js
+++ routes/books.js
@@ -1,8 +1,11 @@
 const { Router } = require("express"); //destrocturing router from express
 const router = new Router();
-const { createBook, getBooks } = require("../controllers/books");
+const { createBook, getBooks, getBookById, updateBookById, deleteBookById } = require("../controllers/books");
 
 router.route("/").post(createBook);
 router.route("/").get(getBooks);
+router.route("/:id").get(getBookById);
+router.route("/:id").put(updateBookById);
+router.route("/:id").delete(deleteBookById); 
 
 module.exports = router;
